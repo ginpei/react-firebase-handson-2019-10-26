09430389ecfd3bf060493b14e05397b332901def
ginpei <ginpei@gmail.com>
2019-10-25 20:27:42 -0700

    Routing の設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
 import HomePage from './screens/HomePage';
+import NotFoundScreen from './screens/NotFoundScreen';
+
+export const appHistory = createBrowserHistory();
 
 const App: React.FC = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router history={appHistory}>
+      <Switch>
+        <Route exact={true} path="/" component={HomePage}/>
+        <Route component={NotFoundScreen}/>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/screens/NotFoundScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundScreen: React.FC = () => {
+  return (
+    <div className="NotFoundScreen">
+      <h1>Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundScreen;
