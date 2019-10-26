513a9865128cff1eb15480fee477ecb7c01fc35d
ginpei <ginpei@gmail.com>
2019-10-26 12:53:03 -0700

    ログインを試す

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -6,6 +6,7 @@ import DefaultLayout from '../basics/DefaultLayout';
 
 const HomePage: React.FC = () => {
   const [message, setMessage] = useState('');
+  const [userId, setUserId] = useState('');
 
   useEffect(() => {
     const firestore = firebase.firestore();
@@ -30,6 +31,29 @@ const HomePage: React.FC = () => {
     await doc.set(data);
   };
 
+  useEffect(() => {
+    const auth = firebase.auth();
+    return auth.onAuthStateChanged(
+      (user) => {
+        if (user) {
+          setUserId(user.uid);
+        } else {
+          setUserId('');
+        }
+      },
+    )
+  }, []);
+
+  const onLogInClick = async () => {
+    const email = 'text@example.com';
+    const password = '123456';
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+  };
+
+  const onLogOutClick = async () => {
+    await firebase.auth().signOut();
+  };
+
   return (
     <DefaultLayout className="HomePage">
       <h1>Home</h1>
@@ -46,6 +70,13 @@ const HomePage: React.FC = () => {
         {message}
       </p>
       <button onClick={onUpdateClick}>Update</button>
+      <p>
+        {'User ID: '}
+        {userId}
+        <br/>
+        <button onClick={onLogInClick}>Log in</button>
+        <button onClick={onLogOutClick}>Log out</button>
+      </p>
       <NiceMarkdown content={`
 # Hello Markdown World!
 
